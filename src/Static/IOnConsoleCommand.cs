f9bdc27dcab572a786c00742503ec4ccb4fd7f77
raul <raulssorban@gmail.com>
2024-03-06 17:30:15 +0100

    Analytics update

--- src/Static/IOnConsoleCommand.cs
+++ src/Static/IOnConsoleCommand.cs
@@ -48,11 +48,7 @@ public partial class Category_Static
 
 					if (Community.Runtime.Config.Misc.oCommandChecks && command.StartsWith("o.") || command.StartsWith("oxide."))
 					{
-						if (Analytic.Enabled)
-						{
-							Analytic.Include("command", command);
-							Analytic.Send("o_command_attempt");
-						}
+						Analytics.o_command_attempt(command, options);
 
 						Logger.Warn($"Oxide commands (o.* or oxide.*) don't work in Carbon. Please use 'c.find c.' to list all available Carbon commands.");
 						return false;
